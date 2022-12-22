80f40d8eab24bf3df45ee7a4ed9a0cc23a006dd8
jerrita <je5r1ta@icloud.com>
2022-12-22 20:17:42 +0800

    Initialize

---
+++ main.py
@@ -0,0 +1,17 @@
+import easyocr
+from PIL import ImageGrab, Image
+
+if __name__ == '__main__':
+    print('Loading models...')
+    reader = easyocr.Reader(['ch_sim', 'en'], gpu='True')
+    while True:
+        input('\nCopy image to your clipboard and press any key to continue: ')
+        img = ImageGrab.grabclipboard()
+        if not isinstance(img, Image.Image):
+            print('This is not a valid image, try again.')
+            continue
+
+        img.save('temp.png')
+        res = reader.readtext('temp.png', detail=0)
+        print(''.join(res))
+
